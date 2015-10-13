ebfc037353d73ee2b1a84626652545c3dde8f0fc
waghsk <waghsbox@gmail.com>
2015-10-12 23:21:42 -0400

    undid data commit

---
+++ install.sh
@@ -0,0 +1,2 @@
+wget https://www.i2b2.org/software/projects/installer/axis2-1.6.2-war.zip	
+curl --create-dirs -L --cookie "oraclelicense=accept-securebackup-cookie; gpw_e24=http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html" http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-x64.tar.gz -o jdk-7u51-linux-x64.tar.gz
