8eeecfb66dbd2325e8a8aabe7255bdb04b737e7c
EC2 Default User <ec2-user@ip-172-31-10-135.ec2.internal>
2016-03-31 21:15:57 +0000

    package install and compile works

---
+++ old/create_database.sql
@@ -0,0 +1,9 @@
+create database i2b2;-- not mentioned in documentation
+\c i2b2;
+#create schema i2b2demodata;
+create schema i2b2hive;
+#create schema i2b2imdata;
+#create schema i2b2metadata;
+#create schema i2b2pm;
+#create schema i2b2workdata;
+
