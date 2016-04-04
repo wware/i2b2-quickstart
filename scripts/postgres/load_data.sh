719826ab70404c163b0b863dbcc34e1866cd3fa1
waghsk <waghsk@gmail.com>
2016-04-04 18:04:14 -0400

    fixed errors in sql

--- scripts/postgres/load_data.sh
+++ scripts/postgres/load_data.sh
@@ -26,13 +26,13 @@ load_demo_data(){
 	mkdir ~/tmp
 	for x in "create_postgresql_i2b2hive_tables.sql" "work_db_lookup_postgresql_insert_data.sql" "ont_db_lookup_postgresql_insert_data.sql" "im_db_lookup_postgresql_insert_data.sql" "crc_db_lookup_postgresql_insert_data.sql"
 	do
-	cat scripts/$x|sql -U i2b2hive i2b2 ;
+	cat scripts/$x|psql -U i2b2hive i2b2 ;
 	done;
 
 	cd ../Pmdata/
 	for x in "create_postgresql_i2b2pm_tables.sql" "create_postgresql_triggers.sql"
 	do echo $x;cat scripts/$x|psql -U i2b2pm i2b2;done;
-	cat scripts/pm_access_insert_data.sql| sed  -e "s/localhost:9090/$IP/" |psql -U i2b2pm i2b2;
+	cat scripts/pm_access_insert_data.sql| sed "s/localhost:9090/$IP/" |psql -U i2b2pm i2b2;
 
 	#echo "grant all privileges on all tables in schema i2b2hive to i2b2hive;"|psql -U postgres i2b2
 
