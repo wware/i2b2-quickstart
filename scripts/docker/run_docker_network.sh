a589ef798f54787a31b6b415f1ef27a0e8a77da9
waghsk <waghsk@github.com>
2016-04-26 02:01:10 -0400

    docker-compose works

--- scripts/docker/run_docker_network.sh
+++ scripts/docker/run_docker_network.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 IP=$1
 
-sudo docker stop i2b2-web; sudo docker rm i2b2-web;
 sudo docker network create i2b2-net
 sudo docker run -d  -p 5432:5432 --net i2b2-net --name i2b2-pg   i2b2/i2b2-pg:0.5
 sudo docker run -d -p 8080:8080 -p 9990:9990 --net i2b2-net --name i2b2-wildfly i2b2/i2b2-wildfly:0.1
-sudo docker run -d  -p 443:443 -p 80:80 --net i2b2-net --name i2b2-web i2b2/i2b2-web:0.95 /run-httpd.sh 192.168.254.147
+sudo docker run -d  -p 443:443 -p 80:80 --net i2b2-net --name i2b2-web i2b2/i2b2-web:0.95 /run-httpd.sh $IP
 
 sleep 5;
 sudo docker exec -it i2b2-pg bash -c "export PUBLIC_IP=$IP;sh update_pm_cell_data.sh; "
 
-~                                                                                                                                                                                
-~                                                                                                                                                                                
-~                                                             
+exit
