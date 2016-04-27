9f487bb407282b6d4ea515dbf49e708cc023f859
Kavishwar Wagholikar <waghsbox@gmail.com>
2016-04-27 15:46:24 -0400

    Update centos_first_docker.sh

--- scripts/docker/centos_first_docker.sh
+++ scripts/docker/centos_first_docker.sh
@@ -1,7 +1,7 @@
 sudo rpm --import "https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e"
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://packages.docker.com/1.10/yum/repo/main/centos/7
-sudo yum install docker-engine
+sudo yum install --nogpgcheck -y docker-engine
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
