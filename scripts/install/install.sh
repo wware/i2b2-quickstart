700617d49afbf21dcf525fdfe98c6989ed487055
Cloud User <centos@ip-172-31-6-150.ec2.internal>
2017-02-22 13:34:02 +0000

    fixed failure to axis2 from i2b2core to war axis war folder

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -186,12 +186,8 @@ copy_axis2_to_wildfly_i2b2war(){
 
 	_FILE=$JBOSS_HOME/standalone/deployments/i2b2.war/WEB-INF/conf/axis2.xml
 	_FROM_FILE="$BASE/unzipped_packages/i2b2-core-server-master/edu.harvard.i2b2.server-common/etc/axis2/axis2.xml"
-	if [ -d $_FILE ] ; then 
-		echo "axis2.xml already copied to i2b2 jboss deploy "
-	else
 		cp $_FROM_FILE $_FILE
 		echo "copying axis2.xml  to i2b2 jboss deploy "
-	fi
 	#cp i2b2-quickstart/unzipped_packages/i2b2-core-server-master/edu.harvard.i2b2.server-common/etc/axis2/axis2.xml $DAPP/jbh/standalone/deployments/i2b2.war/WEB-INF/conf/axis2.xml
 }
 
@@ -234,6 +230,7 @@ compile_i2b2core(){
 #cp i2b2-quickstart/unzipped_packages/i2b2-core-server-master/edu.harvard.i2b2.server-common/etc/axis2/axis2.xml $DAPP/jbh/standalone/deployments/i2b2.war/WEB-INF/conf/axis2.xml
 #cp i2b2-quickstart/unzipped_packages/i2b2-core-server-master/edu.harvard.i2b2.server-common/etc/axis2/axis2.xml $DAPP/jbh/standalone/deployments/i2b2.war/WEB-INF/conf/axis2.xml
 
+copy_axis2_to_wildfly_i2b2war;
 	echo "PWD:$PWD"
 
 	local CELL_NAME="pm"
