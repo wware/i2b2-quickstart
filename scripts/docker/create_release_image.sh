d112c8d070a63e48a64b4422346caf6f0e632c89
Kavishwar Wagholikar <waghsk@gmail.com>
2017-02-26 23:35:16 +0000

    success for pushing wildfly release

--- scripts/docker/create_release_image.sh
+++ scripts/docker/create_release_image.sh
@@ -1,9 +1,21 @@
 BASE=$1
-PAK=i2b2-core-server
-TAG=$(curl https://api.github.com/repos/i2b2/i2b2-core-server/releases/latest | grep "tag_name"|cut -d: -f2|tr -d ",\" ")
-echo $TAG
-TDIR="$BASE/unzipped_packages/$PAK-master/"
-HISTORY_DIR="$BASE/history"
+SUF=$2  #i2b2-core-server
+
+if [ $SUF == "wildfly" ]; then
+	PAK="i2b2-$SUF"
+	PAKTAR="i2b2-core-server" 
+fi
+
+check_pass_set(){
+	_PASS=$1
+	echo " checking $1"
+	if [ ! -v $1 ]; then
+		echo "$_PASS is not set"
+		exit
+	else
+		echo " FOUND $_PASS"
+	fi
+}
 
 create_dir(){
 	_TDIR=$1
@@ -21,6 +33,19 @@ write_history(){
 	echo "$_TAG">"$HISTORY_DIR/$_PAK"
 }
 
+check_pass_set I2B2AUTO_PASS
+check_pass_set DOCKER_CONTENT_TRUST_ROOT_PASSPHRASE
+check_pass_set DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE
+check_pass_set IP_ADD
+check_pass_set PAKTAR
+
+
+TAG=$(curl https://api.github.com/repos/i2b2/$PAKTAR/releases/latest | grep "tag_name"|cut -d: -f2|tr -d ",\" ")
+echo $TAG
+TDIR="$BASE/unzipped_packages/$PAKTAR-master/"
+HISTORY_DIR="$BASE/history"
+
+
 create_dir $TDIR;
 create_dir $HISTORY_DIR;
 
@@ -28,11 +53,24 @@ LAST_TAG=$(cat $HISTORY_DIR/$PAK)
 
 if [ "$LAST_TAG" == "$TAG" ]; then
 	echo "latest release was already processed"
+	exit
 else 
 	cd $TDIR
-	LINK="https://github.com/i2b2/$PAK/archive/$TAG.tar.gz"
+	LINK="https://github.com/i2b2/$PAKTAR/archive/$TAG.tar.gz"
 	echo "downloading $LINK"
 	wget -O downloaded_tar $LINK && write_history $PAK $TAG
 	tar -xvzf downloaded_tar --strip 1
 	rm -rf downloaded_tar
 fi
+
+cd $BASE
+
+rm -rf $BASE/local/docker/$PAK
+docker rmi -f i2b2/$SUF:release-$TAG
+sh $BASE/scripts/docker/create_i2b2_network.sh  $BASE $IP_ADD |tee firstlog
+
+IMG=$(docker images i2b2/$SUF --format {{.ID}})
+docker tag $IMG i2b2/$PAK:release-$TAG
+docker login -u i2b2auto -p "$I2B2AUTO_PASS"
+docker push i2b2/$PAK:release-$TAG
+
