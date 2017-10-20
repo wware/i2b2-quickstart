5dce944c4c4e5b059f89d168017e3b3ca93a9dad
kmmmullins <kmmmullins@gmail.com>
2017-10-20 10:24:14 -0400

    added variables for local and used full paths for scripts

--- scripts/install/centos_first_install.sh
+++ scripts/install/centos_first_install.sh
@@ -4,6 +4,7 @@ IP=$1
 
 if [[ $IP ]];then
 	echo "using given IP"
+	IP90="$IP:9090"
 else
 	IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 	IP90="$IP:9090"
@@ -15,29 +16,38 @@ sudo yum -y install ant tar sed bzip2 git php perl wget zip unzip httpd patch
 setenforce 0
 service iptables stop
  
-sudo -u nobody bash -c : && RUNAS="sudo -u $SUDO_USER"
-
 LLOCAL=/opt/local
 LLOCALGIT=/opt/local/git
 mkdir $LLOCAL
 mkdir $LLOCALGIT
-
 cd $LLOCALGIT
 
+sudo -u nobody bash -c : && RUNAS="sudo -u $SUDO_USER"
 $RUNAS bash << _
 git clone https://github.com/kmullins/i2b2-quickstart.git 
 
 cd $LLOCAL
+pwd
 
 source $LLOCALGIT/i2b2-quickstart/scripts/install/install.sh
+if [$? -ne 0]
+then
+	echo "#############  Problem in install script ##########"
+fi
 
 download_i2b2_source $LLOCAL
+if [$? -ne 0]
+then
+	echo "#############  Problem with download_12b2_sources ##########"
+fi
+
+
 unzip_i2b2core $LLOCAL
+if [$? -ne 0]
+then
+	echo "#############  Problem with unzip i2b2core ##########"
+fi
 _
-exit
-exit
-
-
 
 
 
@@ -53,15 +63,18 @@ $RUNAS bash << _
 if psql -U postgres -lqt | cut -d \| -f 1 |grep "i2b2";then
 	echo "i2b2 db exists in postgres"
 else
-	source scripts/postgres/load_data.sh $(pwd)
+	source $LLOCALGIT/i2b2-quickstart/scripts/postgres/load_data.sh $(pwd)
 	create_db_schema $(pwd) "-U postgres";
 	load_demo_data $(pwd) " -d i2b2 " $IP 
 fi
 _
 
 $RUNAS bash << _
-source scripts/install/install.sh
+source $LLOCALGIT/i2b2-quickstart/scripts/install/install.sh
 check_homes_for_install $(pwd)
 compile_i2b2core $(pwd)  
 run_wildfly $(pwd)
-_
+
+
+
+
