9c14c4690e864c463c96526974511a3e09e0eb2e
Kevin Mullins <kmmmullins@gmail.com>
2017-11-27 10:10:02 -0500

    changed docker version

--- scripts/docker/centos_first_docker.sh
+++ scripts/docker/centos_first_docker.sh
@@ -9,7 +9,7 @@ else
     echo "installing docker" 
       sudo rpm --import "https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e"
       sudo yum install -y yum-utils
-      sudo yum-config-manager --add-repo https://packages.docker.com/1.10/yum/repo/main/centos/7
+      sudo yum-config-manager --add-repo https://packages.docker.com/1.13/yum/repo/main/centos/7
       sudo yum install --nogpgcheck -y docker-engine
       sudo systemctl enable docker.service
       sudo systemctl start docker.service
